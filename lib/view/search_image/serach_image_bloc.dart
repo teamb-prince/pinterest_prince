e97d849cd5aec9eeebc8bf198ffd9cf82b5bfd8c
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-09 19:15:38 +0900

    add SearchImageBloc

---
+++ lib/view/search_image/serach_image_bloc.dart
@@ -0,0 +1,65 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pintersest_clone/data/image_repository.dart';
+import 'package:pintersest_clone/model/image_model.dart';
+
+abstract class SearchImageEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class RequestSearch extends SearchImageEvent {
+  final String url;
+
+  RequestSearch(this.url);
+}
+
+abstract class SearchImageState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class InitialState extends SearchImageState {}
+
+class LoadingState extends SearchImageState {}
+
+class LoadedState extends SearchImageState {
+  final ImageModel imageModel;
+
+  LoadedState(this.imageModel);
+
+  @override
+  List<Object> get props => [imageModel];
+}
+
+class NoImageState extends SearchImageState {}
+
+class ErrorState extends SearchImageState {
+  final Exception exception;
+
+  ErrorState(this.exception);
+
+  @override
+  List<Object> get props => [exception];
+}
+
+class SearchImageBloc extends Bloc<SearchImageEvent, SearchImageState> {
+  final ImageRepository _imageRepository;
+
+  SearchImageBloc(this._imageRepository);
+
+  @override
+  SearchImageState get initialState => InitialState();
+
+  @override
+  Stream<SearchImageState> mapEventToState(SearchImageEvent event) async* {
+    if (event is RequestSearch) {
+      yield LoadingState();
+      try {
+        yield LoadedState(await _imageRepository.searchImageFromUrl(event.url));
+      } catch(e) {
+        ErrorState(e);
+      }
+    }
+  }
+}
