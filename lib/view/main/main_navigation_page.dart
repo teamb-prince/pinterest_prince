95c21c3fe0d5f41937243cf50c088e5e1afbd068
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-11 16:14:41 +0900

    add BottomNavigationBar

---
+++ lib/view/main/main_navigation_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:pintersest_clone/view/main/account_widget/account_widget.dart';
+import 'package:pintersest_clone/view/main/home_widget/home_widget.dart';
+import 'package:pintersest_clone/view/main/notification_widget/notification_widget.dart';
+import 'package:pintersest_clone/view/main/saerch_widget/serach_widget.dart';
+
+class MainPageDestination {
+  final int index;
+  final String title;
+  final Widget body;
+  final IconData iconData;
+
+  MainPageDestination(this.index, this.title, this.body, this.iconData);
+}
+
+List<MainPageDestination> mainPageDestinations = [
+  MainPageDestination(0, "Discover", HomeWidget(), Icons.home),
+  MainPageDestination(1, "Library", SearchWidget(), Icons.search),
+  MainPageDestination(2, "Search", NotificationWidget(), Icons.notifications),
+  MainPageDestination(3, "Account", AccountWidget(), Icons.person),
+];
+
+class MainNavigationPage extends StatefulWidget {
+  @override
+  _MainNavigationPageState createState() => _MainNavigationPageState();
+}
+
+class _MainNavigationPageState extends State<MainNavigationPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: mainPageDestinations[_currentIndex].body,
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Material(
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: mainPageDestinations
+            .map((item) => BottomNavigationBarItem(
+                  icon: Icon(item.iconData),
+                  title: Text(item.title),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
