760f920020e1c1514f3616d2b53d7b98c5319370
yaya1026 <yaya.w.1026@gmail.com>
2020-06-15 23:49:49 +0900

    fix crawling_image_widget ui

--- lib/view/main/crawling_image/crawling_image_widget.dart
+++ lib/view/main/crawling_image/crawling_image_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pintersest_clone/app_route.dart';
 import 'package:pintersest_clone/data/image_repository.dart';
+import 'package:pintersest_clone/values/app_colors.dart';
 import 'package:pintersest_clone/view/main/crawling_image/bloc/crawling_image_event.dart';
 import 'package:pintersest_clone/view/main/crawling_image/bloc/crawling_image_state.dart';
 
@@ -18,6 +20,9 @@ class CrawlingImageWidget extends StatefulWidget {
 }
 
 class _CrawlingImageState extends State<CrawlingImageWidget> {
+  int _selectedIndex = 0;
+  double _footerHeight = 48.0;
+
   @override
   Widget build(BuildContext context) {
     final CrawlingImageArgs args = ModalRoute.of(context).settings.arguments;
@@ -31,46 +36,95 @@ class _CrawlingImageState extends State<CrawlingImageWidget> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              children: [
-                _buildGetImage(),
-              ],
-            ),
-          ),
+          child: BlocBuilder<CrawlingImageBloc, CrawlingImageState>(
+              builder: (context, state) {
+            if (state is LoadedState) {
+              final List<String> imageUrls = state.imageModel.imageUrls;
+              return Container(
+                child: Column(
+                  children: [
+                    Expanded(child: _buildGetImage(imageUrls)),
+                    _buildFooter(),
+                  ],
+                ),
+              );
+            } else if (state is NoImageState) {
+              return Text("No image.");
+            } else if (state is ErrorState) {
+              return Text(state.exception.toString());
+            } else if (state is LoadingState) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is InitialState) {
+              return Text("Initial state.");
+            }
+            return Container();
+          }),
         ),
       ),
     );
   }
 
-  Widget _buildGetImage() {
+  Widget _buildGetImage(List<String> imageUrls) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: BlocBuilder<CrawlingImageBloc, CrawlingImageState>(
-          builder: (context, state) {
-        if (state is LoadedState) {
-          final List<String> imageUrls = state.imageModel.imageUrls;
-          return GridView.count(
-            crossAxisCount: 2,
-            shrinkWrap: true,
-            children: List.generate(imageUrls.length, (index) {
-              return Image.network(
-                imageUrls[index],
-                fit: BoxFit.cover,
-              );
-            }),
+      child: GridView.count(
+        crossAxisCount: 2,
+        shrinkWrap: true,
+        children: List.generate(imageUrls.length, (index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            child: Stack(
+              children: [
+                Image.network(
+                  imageUrls[index],
+                  fit: BoxFit.cover,
+                ),
+                _selectedIndex == index ? _buildCheckbox() : Container(),
+              ],
+            ),
           );
-        } else if (state is NoImageState) {
-          return Text("No image.");
-        } else if (state is ErrorState) {
-          return Text(state.exception.toString());
-        } else if (state is LoadingState) {
-          return CircularProgressIndicator();
-        } else if (state is InitialState) {
-          return Text("Initial state.");
-        }
-        return Container();
-      }),
+        }),
+      ),
+    );
+  }
+
+  Widget _buildCheckbox() {
+    return Positioned(
+      top: 4,
+      left: 4,
+      child: Icon(
+        Icons.check_circle,
+        color: AppColors.red,
+      ),
+    );
+  }
+
+  Widget _buildFooter() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+        height: _footerHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            RaisedButton(
+              child: Text(
+                "保存", // TODO Pinのボードへの保存処理はDBが固まってから
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: AppColors.white),
+              ),
+              color: AppColors.red,
+              onPressed: () {
+                Navigator.popUntil(context, ModalRoute.withName(AppRoute.home));
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
