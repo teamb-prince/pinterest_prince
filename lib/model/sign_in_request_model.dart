15c1507286313c2750b7b2faf27a8fde7c56a156
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-18 16:24:18 +0900

    rename LoginRequestModel to SignInRequestModel

--- lib/model/sign_in_request_model.dart
+++ lib/model/sign_in_request_model.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 
-class LoginRequestModel extends Equatable {
+class SignInRequestModel extends Equatable {
   final String email;
   final String password;
 
-  const LoginRequestModel({@required this.email, @required this.password});
+  const SignInRequestModel({@required this.email, @required this.password});
 
   String toJson() {
     return jsonEncode({
