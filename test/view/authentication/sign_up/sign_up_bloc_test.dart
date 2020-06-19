fedbd289e7fe7f959ba8268ef35ed4964b6abd00
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-19 14:25:11 +0900

    add some tests for SignUpBloc

---
+++ test/view/authentication/sign_up/sign_up_bloc_test.dart
@@ -0,0 +1,44 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pintersest_clone/model/sign_up_request_model.dart';
+import 'package:pintersest_clone/view/authentication/sign_up_widget/bloc/sign_up_bloc.dart';
+import 'package:pintersest_clone/view/authentication/sign_up_widget/bloc/sign_up_event.dart';
+import 'package:pintersest_clone/view/authentication/sign_up_widget/bloc/sign_up_state.dart';
+
+import '../login/login_bloc_test.dart';
+
+final SignUpRequestModel mockSignUpRequest = SignUpRequestModel(
+    email: 'test@email.com', password: 'password', confirmPassword: 'password');
+
+void main() {
+  MockAuthRepository mockAuthRepository;
+
+  group('SignUpBloc test', () {
+    setUp(() async {
+      mockAuthRepository = MockAuthRepository();
+    });
+
+    blocTest(
+        'emit [InitialState(), LoadingState(), SuccessState()] when sign up request will succeed',
+        build: () async {
+      when(mockAuthRepository.signUp(any))
+          .thenAnswer((_) => Future.value(null));
+
+      return SignUpBloc(mockAuthRepository);
+    }, act: (bloc) {
+      bloc.add(SignUp(signUpRequestModel: mockSignUpRequest));
+    }, skip: 0, expect: [InitialState(), LoadingState(), SuccessState()]);
+
+    blocTest(
+        'emit [InitialState(), LoadingState(), ErrorState()] when sign up request will fail',
+        build: () async {
+          when(mockAuthRepository.signUp(any))
+              .thenAnswer((_) => Future.error(Exception()));
+
+          return SignUpBloc(mockAuthRepository);
+        }, act: (bloc) {
+      bloc.add(SignUp(signUpRequestModel: mockSignUpRequest));
+    }, skip: 0, expect: [InitialState(), LoadingState(), isA<ErrorState>()]);
+  });
+}
