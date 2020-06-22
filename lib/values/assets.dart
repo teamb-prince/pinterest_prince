aa625c62e6ec1bd8b4a51a9943960a64befce02b
yaya1026 <yaya.w.1026@gmail.com>
2020-06-22 17:55:01 +0900

    add board test

--- lib/values/assets.dart
+++ lib/values/assets.dart
@@ -1,6 +1,9 @@
 class Assets {
   static const String assetsPath = 'assets';
+  static const String mockAssetsPath = '$assetsPath/mock';
   static const String imagePath = '$assetsPath/images';
 
   static const String headerImage = '$imagePath/login_image.png';
+
+  static const String testBoardResponse = '$mockAssetsPath/test_board.json';
 }
