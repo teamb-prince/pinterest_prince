9031f7981af3512b597881a53dba53d66977202e
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-22 17:42:04 +0900

    add test for ImageModel

--- lib/values/assets.dart
+++ lib/values/assets.dart
@@ -6,4 +6,5 @@ class Assets {
   static const String headerImage = '$imagePath/login_image.png';
 
   static const String testUserResponse = '$mockAssetsPath/test_user.json';
+  static const String testImagesResponse = '$mockAssetsPath/test_images.json';
 }
