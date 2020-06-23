307f4c2454a41787a63c2912724845dccb98144d
yaya1026 <yaya.w.1026@gmail.com>
2020-06-23 15:54:43 +0900

    fix data's linter error'

--- lib/data/pins_repository.dart
+++ lib/data/pins_repository.dart
@@ -2,9 +2,9 @@ import 'package:pintersest_clone/api/pins_api.dart';
 import 'package:pintersest_clone/model/pin_model.dart';
 
 class PinsRepository {
-  final PinsApi _pinsApi;
-
   PinsRepository(this._pinsApi);
+
+  final PinsApi _pinsApi;
 
   Future<PinModel> getPin(String id) => _pinsApi.getPin(id);
 
