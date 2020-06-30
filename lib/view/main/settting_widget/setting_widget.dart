a1455d79d1466129562ef379bc71a646396627ac
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-30 15:50:48 +0900

    add navigation when user logged out

--- lib/view/main/settting_widget/setting_widget.dart
+++ lib/view/main/settting_widget/setting_widget.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pintersest_clone/app_route.dart';
 import 'package:pintersest_clone/data/auth_repository.dart';
 import 'package:pintersest_clone/values/app_colors.dart';
 import 'package:pintersest_clone/view/main/settting_widget/bloc/setting_bloc.dart';
+import 'package:pintersest_clone/view/main/settting_widget/bloc/setting_event.dart';
 import 'package:pintersest_clone/view/main/settting_widget/bloc/setting_state.dart';
 
 class SettingItem {
-  SettingItem({@required this.iconData, @required this.text});
+  SettingItem(
+      {@required this.iconData, @required this.text, @required this.event});
 
   final IconData iconData;
   final String text;
+  final SettingEvent event;
 }
 
 final List<SettingItem> settingItems = [
-  SettingItem(iconData: Icons.power_settings_new, text: 'ログアウト')
+  SettingItem(
+      iconData: Icons.power_settings_new, text: 'ログアウト', event: SignOut())
 ];
 
 class SettingWidget extends StatelessWidget {
@@ -26,40 +31,41 @@ class SettingWidget extends StatelessWidget {
   }
 
   Widget _buildScreen(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '設定',
-          style: TextStyle(color: AppColors.black),
-        ),
-        brightness: Brightness.light,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: AppColors.black),
-      ),
-      body: _buildListView(context),
-    );
-  }
-
-  Widget _buildListView(BuildContext context) {
     return BlocListener<SettingBloc, SettingState>(
         listener: (context, state) {
           if (state is SuccessState) {
-            print('logged out');
+            Navigator.of(context).pushNamedAndRemoveUntil(
+                AppRoute.loginTop, (route) => false);
           }
         },
-        child: Container(
-          child: ListView.builder(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              '設定',
+              style: TextStyle(color: AppColors.black),
+            ),
+            brightness: Brightness.light,
+            elevation: 0,
+            backgroundColor: Colors.white,
+            iconTheme: const IconThemeData(color: AppColors.black),
+          ),
+          body: _buildListView(context),
+        ));
+  }
+
+  Widget _buildListView(BuildContext context) {
+    return Container(
+        child: ListView.builder(
             itemBuilder: (context, index) =>
                 _buildListTile(context, settingItems[index]),
-            itemCount: settingItems.length,
-          ),
-        ));
+            itemCount: settingItems.length));
   }
 
   Widget _buildListTile(BuildContext context, SettingItem settingItem) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        BlocProvider.of<SettingBloc>(context).add(settingItem.event);
+      },
       child: ListTile(
         leading: Icon(settingItem.iconData),
         title: Text(settingItem.text,
