05e07afcc7b71b182b7f60f6da9533f31127d36b
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-30 14:20:11 +0900

    add SettingWidget and routing for it

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,18 +1,24 @@
 class AppRoute {
-  static const String home = '/home';
-  static const String pinDetail = '/pin_detail';
-  static const String userDetail = '/user_detail';
-  static const String inputUrl = '/input_url';
-  static const String crawlingImage = '/crawling_image';
+  // Authentication
   static const String loginTop = '/login_top';
-  static const String createAccount = '/create_account';
   static const String login = '/login';
+  static const String loginForm = '/login_form';
+  static const String createAccount = '/create_account';
+  static const String signupForm = '/signup_form';
+
+  // Main
+  static const String home = '/home';
+  static const String userDetail = '/user_detail';
+  static const String pinDetail = '/pin_detail';
+  static const String boardDetail = '/board_detail';
   static const String createPin = '/create_pin';
   static const String createBoard = '/create_board';
+  static const String inputUrl = '/input_url';
+  static const String crawlingImage = '/crawling_image';
   static const String editCrawlingImage = '/edit_crawling_image';
   static const String selectBoardFromLocal = '/select_board_from_local';
   static const String selectBoardFromUrl = '/select_board_from_url';
-  static const String signupForm = '/signup_form';
-  static const String loginForm = '/login_form';
-  static const String boardDetail = '/board_detail';
+
+  // Others
+  static const String setting = '/setting';
 }
