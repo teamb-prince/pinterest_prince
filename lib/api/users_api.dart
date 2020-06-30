31dcc4d406f7b4f528fdc82febce2ad60e6a8723
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-06-30 10:37:56 +0900

    add UsersApi
    rename UserApi to UsersApi

---
+++ lib/api/users_api.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:pintersest_clone/api/api_client.dart';
+import 'package:pintersest_clone/model/user_model.dart';
+
+abstract class UsersApi {
+  Future<UserModel> getAccountInfo();
+}
+
+class DefaultUsersApi extends UsersApi {
+  DefaultUsersApi(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  @override
+  Future<UserModel> getAccountInfo() async {
+    final response = await _apiClient.get('/user/profile');
+    return UserModel.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  }
+}
