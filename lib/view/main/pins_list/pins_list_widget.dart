b12be0bf5c3f27e03549be717e86f36f4263a35d
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-07-01 16:48:38 +0900

    fix widgets which display a list of pins

--- lib/view/main/pins_list/pins_list_widget.dart
+++ lib/view/main/pins_list/pins_list_widget.dart
@@ -78,7 +78,8 @@ class _PinsListWidgetState extends State<PinsListWidget> {
               crossAxisCount: 4,
               crossAxisSpacing: 8,
               mainAxisSpacing: 8,
-              itemBuilder: (context, index) => PinTile(pin: pins[index]),
+              itemBuilder: (context, index) =>
+                  PinTile(pin: pins[index], heroTag: pins[index].id),
               staggeredTileBuilder: (index) => const StaggeredTile.fit(2),
               itemCount: pins.length,
             ),
