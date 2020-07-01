beb068edf302805616d6a0c322bb7550dc2e9619
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-07-01 13:40:07 +0900

    fix AccountWidget to display appbar even when token is invalid

--- lib/view/main/account_widget/account_widget.dart
+++ lib/view/main/account_widget/account_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pintersest_clone/app_route.dart';
 import 'package:pintersest_clone/data/users_repository.dart';
-import 'package:pintersest_clone/model/user_model.dart';
 import 'package:pintersest_clone/values/app_colors.dart';
 import 'package:pintersest_clone/view/common_widget/rounded_tab_indicator.dart';
 import 'package:pintersest_clone/view/main/boards_list_widget/boards_list_widget.dart';
@@ -24,69 +23,71 @@ class AccountWidget extends StatelessWidget {
   }
 
   Widget _buildScreen(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'アカウント',
+            style: TextStyle(color: AppColors.black),
+          ),
+          brightness: Brightness.light,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: _buildAccountIcon(context),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoute.setting);
+              },
+              icon: Icon(Icons.settings, color: AppColors.black),
+            )
+          ],
+          bottom: const TabBar(
+            indicator: RoundedTabIndicator(height: 40, color: AppColors.black),
+            labelColor: AppColors.white,
+            unselectedLabelColor: AppColors.black,
+            isScrollable: true,
+            labelStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            labelPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+            tabs: <Widget>[
+              Tab(text: 'ボード'),
+              Tab(text: 'ピン'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            BoardsListWidget(),
+            PinsListWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAccountIcon(BuildContext context) {
     return BlocBuilder<AccountBloc, AccountState>(builder: (context, state) {
       if (state is LoadedState) {
-        return DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'アカウント',
-                style: TextStyle(color: AppColors.black),
-              ),
-              brightness: Brightness.light,
-              elevation: 0,
-              backgroundColor: Colors.white,
-              leading: _buildAccountIcon(context, state.user),
-              actions: <Widget>[
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AppRoute.setting);
-                  },
-                  icon: Icon(Icons.settings, color: AppColors.black),
-                )
-              ],
-              bottom: const TabBar(
-                indicator:
-                    RoundedTabIndicator(height: 40, color: AppColors.black),
-                labelColor: AppColors.white,
-                unselectedLabelColor: AppColors.black,
-                isScrollable: true,
-                labelStyle:
-                    TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                labelPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 12),
-                tabs: <Widget>[
-                  Tab(text: 'ボード'),
-                  Tab(text: 'ピン'),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: <Widget>[
-                BoardsListWidget(),
-                PinsListWidget(),
-              ],
+        return GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, AppRoute.userDetail,
+                arguments: UserDetailWidgetArguments(state.user));
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: CircleAvatar(
+              backgroundColor: AppColors.grey,
+              backgroundImage: Image.network(state.user.profileImageUrl).image,
             ),
           ),
         );
       }
-      return Container();
+      return Container(
+          padding: const EdgeInsets.all(8),
+          child: CircleAvatar(
+            backgroundColor: AppColors.grey,
+          ));
     });
-  }
-
-  Widget _buildAccountIcon(BuildContext context, UserModel user) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, AppRoute.userDetail,
-            arguments: UserDetailWidgetArguments(user));
-      },
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        child: CircleAvatar(
-          backgroundColor: AppColors.grey,
-          backgroundImage: Image.network(user.profileImageUrl).image,
-        ),
-      ),
-    );
   }
 }
