4c46889639954859ee066fd82a02f10a78e9300b
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-07-02 11:43:23 +0900

    add animation to appear and disappear bottom nav bar

--- lib/view/main/main_navigation_page.dart
+++ lib/view/main/main_navigation_page.dart
@@ -6,19 +6,18 @@ import 'package:pintersest_clone/view/main/notification/notification_widget.dart
 import 'package:pintersest_clone/view/main/search/search_widget.dart';
 
 class MainPageDestination {
-  MainPageDestination(this.index, this.title, this.body, this.iconData);
+  MainPageDestination(this.index, this.title, this.iconData);
 
   final int index;
   final String title;
-  final Widget body;
   final IconData iconData;
 }
 
 List<MainPageDestination> mainPageDestinations = [
-  MainPageDestination(0, 'ホーム', HomeWidget(), Icons.home),
-  MainPageDestination(1, '検索', SearchWidget(), Icons.search),
-  MainPageDestination(2, 'お知らせ', NotificationWidget(), Icons.message),
-  MainPageDestination(3, '保存済み', AccountWidget(), Icons.person),
+  MainPageDestination(0, 'ホーム', Icons.home),
+  MainPageDestination(1, '検索', Icons.search),
+  MainPageDestination(2, 'お知らせ', Icons.message),
+  MainPageDestination(3, '保存済み', Icons.person),
 ];
 
 class MainNavigationPage extends StatefulWidget {
@@ -28,12 +27,19 @@ class MainNavigationPage extends StatefulWidget {
 
 class _MainNavigationPageState extends State<MainNavigationPage> {
   int _currentIndex = 0;
+  bool _isVisible = true;
   final double _bottomNavBarPosition = 32;
   final double _bottomNavBarCornerRadius = 32;
   final double _bottomNavBarHorizontalPadding = 70;
 
   final double _selectedIconSize = 25;
   final double _unselectedIconSize = 20;
+
+  void _updateIsVisible(bool isVisible) {
+    setState(() {
+      _isVisible = isVisible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,15 +48,18 @@ class _MainNavigationPageState extends State<MainNavigationPage> {
       children: <Widget>[
         IndexedStack(
             index: _currentIndex,
-            children: mainPageDestinations
-                .map((destination) => destination.body)
-                .toList()),
+            children: <Widget>[
+              HomeWidget(appearBottomNavBar: _updateIsVisible),
+              SearchWidget(),
+              NotificationWidget(),
+              AccountWidget(),
+            ],),
         Positioned(
           left: 0,
           right: 0,
           bottom: _bottomNavBarPosition,
           child: _buildBottomNavigationBar(context),
-        )
+        ),
       ],
     ));
   }
@@ -59,38 +68,36 @@ class _MainNavigationPageState extends State<MainNavigationPage> {
     return MediaQuery.removePadding(
       context: context,
       removeBottom: true,
-      child: Container(
-        padding:
-            EdgeInsets.symmetric(horizontal: _bottomNavBarHorizontalPadding),
-        child: ClipRRect(
-          borderRadius:
-              BorderRadius.all(Radius.circular(_bottomNavBarCornerRadius)),
-          child: BottomNavigationBar(
-            backgroundColor: AppColors.white,
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: AppColors.black,
-            unselectedItemColor: Colors.grey[500],
-            selectedIconTheme: IconThemeData(size: _selectedIconSize),
-            unselectedIconTheme: IconThemeData(size: _unselectedIconSize),
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            currentIndex: _currentIndex,
-            items: mainPageDestinations
-                .map((item) => BottomNavigationBarItem(
-                      icon: Icon(
-                        item.iconData,
-                      ),
-                      title: Text(
-                        item.title,
-                        style: TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
-                    ))
-                .toList(),
+      child: AnimatedOpacity(
+        opacity: _isVisible ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 500),
+        child: Container(
+          padding:
+              EdgeInsets.symmetric(horizontal: _bottomNavBarHorizontalPadding),
+          child: ClipRRect(
+            borderRadius:
+                BorderRadius.all(Radius.circular(_bottomNavBarCornerRadius)),
+            child: BottomNavigationBar(
+              backgroundColor: AppColors.white,
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: AppColors.black,
+              unselectedItemColor: Colors.grey[500],
+              selectedIconTheme: IconThemeData(size: _selectedIconSize),
+              unselectedIconTheme: IconThemeData(size: _unselectedIconSize),
+              onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              currentIndex: _currentIndex,
+              items: mainPageDestinations
+                  .map((item) => BottomNavigationBarItem(
+                        icon: Icon(item.iconData),
+                        title: Text(item.title,
+                            style: const TextStyle(fontSize: 12)),
+                      ))
+                  .toList(),
+            ),
           ),
         ),
       ),
